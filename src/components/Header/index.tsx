3daec3c430bef1d2075eb712099a91970c12f1d7
denismend <denisladeira1@gmail.com>
2021-06-10 20:53:58 -0300

    feat: header component and global style

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.header__container}>
+      <div className={styles.header__content}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
