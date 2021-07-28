b02777cd69072f7f87f0998ae8784d53649cc9be
denismend <denisladeira1@gmail.com>
2021-07-28 19:35:46 -0300

    style: header bar link status

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
+import { useRouter } from "next/router";
+
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
+import { ActiveLink } from "../ActiveLink";
+
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.header__container}>
       <div className={styles.header__content}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a href="">Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
