1b206a090bf6f04cd2532cf581569a86224ff735
denismend <denisladeira1@gmail.com>
2021-08-06 22:11:43 -0300

    feat: post and post preview page

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,6 +15,39 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscripion = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscripion,
+        };
+      } catch (err) {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
 
@@ -22,29 +55,15 @@ export default NextAuth({
         await fauna.query(
           q.If(
             q.Not(
-              q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(email)
-                )
-              )
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
             ),
-            q.Create(
-              q.Collection("users"), 
-              {
-                data: {
-                  email,
-                },
-              }
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(email)
-              )
-            )
+            q.Create(q.Collection("users"), {
+              data: {
+                email,
+              },
+            }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
           )
-
         );
         return true;
       } catch {
